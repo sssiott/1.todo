71a8505a2cc2e98944cee0b6b43452c0fe8c26ca
sssiott <rpkrm25@gmail.com>
2023-01-17 21:50:36 +0900

    다크모드 지원

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 export default function App1() {
   const [filter, setFilter] = useState(status[0].stat);
   return (
-    <>
+    <DarkModeProvider>
       <Nav statList={status} filter={filter} onFilterChange={setFilter} />
       <Main filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "./Nav.module.css";
+import {HiMoon, HiSun} from 'react-icons/hi';
+import { useDarkMode } from "../context/DarkModeContext";
 
 export default function Nav({ statList, filter, onFilterChange }) {
   const handlefilter = (e) => {
     onFilterChange(e.target.value);
   };
+  const {darkMode, toggleDarkMode} = useDarkMode();
+
 
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {darkMode ? <HiSun/> : <HiMoon/>}
+      </button>
       <ul className={styles.list}>
         {statList.map((i) => (
           <li key={i.code}>
